9b1ea75ec6f52cf78124b86b384f12bdd9da9096
Adam Carr <acarr@jhu.edu>
2022-12-06 11:34:34 -0500

    Day 5 part 2

--- day5.py
+++ day5.py
@@ -1,11 +1,17 @@
-stacks = [["B", "S", "J", "Z", "V", "D", "G"], ["P", "V", "G", "M", "S", "Z"], ["F", "Q", "T", "W", "S", "B", "L", "C"],
+stacks1 = [["B", "S", "J", "Z", "V", "D", "G"], ["P", "V", "G", "M", "S", "Z"], ["F", "Q", "T", "W", "S", "B", "L", "C"],
           ["Q", "V", "R", "M", "W", "G", "J", "H"], ["D", "M", "F", "N", "S", "L", "C"], ["D", "C", "G", "R"],
           ["Q", "S", "D", "J", "R", "T", "G", "H"], ["V", "F", "P"], ["J", "T", "S", "R", "D"]]
 
-#PART 1
+stacks2 = [["B", "S", "J", "Z", "V", "D", "G"], ["P", "V", "G", "M", "S", "Z"], ["F", "Q", "T", "W", "S", "B", "L", "C"],
+          ["Q", "V", "R", "M", "W", "G", "J", "H"], ["D", "M", "F", "N", "S", "L", "C"], ["D", "C", "G", "R"],
+          ["Q", "S", "D", "J", "R", "T", "G", "H"], ["V", "F", "P"], ["J", "T", "S", "R", "D"]]
+
+
 with open('./inputs/day5.txt') as f:
     inputString = f.read()
     moves = inputString.split("\n")
+
+    # PART 1
 
     for move in moves:
         move = move.replace('move ', '')
@@ -17,17 +23,42 @@ with open('./inputs/day5.txt') as f:
         step = 1
 
         while step <= int(sequence[0]):
-            target = stacks[int(sequence[2])-1]
-            origin = stacks[int(sequence[1])-1]
+            target = stacks1[int(sequence[2])-1]
+            origin = stacks1[int(sequence[1])-1]
 
             target.insert(0, origin.pop(0))
 
             step += 1
 
-result = "RESULT: "
-for stack in stacks:
-    result = result + str(stack[0])
+    #PART 2
+    for move in moves:
+        move = move.replace('move ', '')
+        move = move.replace('from ', '')
+        move = move.replace('to ', '')
 
-print(result)
+        sequence = move.split(' ')
 
-#PART 2
+        print(sequence)
+
+        step = 1
+
+        while step <= int(sequence[0]):
+            target = stacks2[int(sequence[2])-1]
+            origin = stacks2[int(sequence[1])-1]
+
+            target.insert(step-1, origin.pop(0))
+
+            step += 1
+
+result1 = "Part 1 RESULT: "
+for stack in stacks1:
+    result1 = result1 + str(stack[0])
+
+print(result1)
+
+result2 = "Part 2 RESULT: "
+
+for stack in stacks2:
+    result2 = result2 + str(stack[0])
+
+print(result2)
