7a6c8850f49aceed6bca5a84eb8ca5a7645082cf
Adam Carr <acarr@jhu.edu>
2022-12-09 00:03:23 -0500

    day 7 WIP

--- day7.py
+++ day7.py
@@ -1,15 +1,46 @@
-with open('./inputs/day6.txt') as f:
-    inputStrings = f.readlines()
-    prevLine = ''
-    currentDir = ''
-    dirSizes = {}
-    # loop through lines
-        # if line is $ ls
-            # pars out the dir name from the cd command stored in prevLine and add it as a key to dirSizes
-            # set current dir to the dir we just created the key for
-        #parse line by space
-        # if first index contains a number
-            # add it to the value indexed to the matching key in dirSizes
+class NonBinTree:
 
-    print(dirSizes)
+    def __init__(self, val):
+        self.val = [val]
+        self.size = 0
+        self.nodes = []
+
+    def add_node(self, val):
+        self.nodes.append(NonBinTree(val))
+
+    def increase_size(self, num):
+        self.size = self.size + num
+
+    def __repr__(self):
+        return f"NonBinTree({self.val}{self.size}): \n {self.nodes}"
+
+
+fileTree = None
+nodePath = []
+currentNode = None
+
+with open('./inputs/day7.txt') as f:
+    inputLines = f.readlines()
+
+    for line in inputLines:
+        if line.startswith('$ cd'):
+            if line != '$ cd ..\n':
+                dir = line.split(' ')[2].replace('\n', '')
+                if fileTree is None:
+                    fileTree = NonBinTree(dir)
+                    currentNode = fileTree
+                else:
+                    currentNode.add_node(dir)
+                    currentNode = currentNode.nodes[len(currentNode.nodes)-1]
+        elif line == '$ ls':
+            break
+        else:
+            lineParts = line.split(' ')
+            if lineParts[0].isnumeric():
+                currentNode.size = currentNode.size + int(lineParts[0])
+
+
+print(fileTree)
+
+
 
